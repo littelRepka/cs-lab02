b10f008b4232e047d67d0a3f03eedad817b89bfd
Bob (MatantsevID) <ivan.kvadrat.kub@gmail.com>
2024-07-08 00:01:47 +0300

    code: add a*b

--- main.cpp
+++ main.cpp
@@ -5,5 +5,6 @@ int main(){
     int a, b;
     cin >> a >> b;
     cout << "A + B = " << a + b << '\n'
-    << "A - B = " << a - b << '\n';
+    << "A - B = " << a - b << '\n'
+    << "A * B = " << a * b << '\n';
 }
